00ff602408d23d5ed23f667696449c4024901e4b
Olesya <olesya.kiril2014@gmail.com>
2024-11-11 18:05:13 +0700

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import store from './store';  // Импортируем наш store
+import store from './store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>  {/* Оборачиваем приложение в Provider */}
+    <Provider store={store}>
       <App />
     </Provider>
   </StrictMode>
